dfb60965cba7427101f0d1b3fd2ff896b626453d
Yasukastsu.A <handbuuuu@gmail.com>
2020-05-07 23:29:35 +0900

    CSS Transitionを利用したアニメーションのイベントを取得する

--- css-animation/main.js
+++ css-animation/main.js
@@ -1,9 +1,26 @@
 window.onload = function() {
   /** jQueryの処理 */
-  var $box = $('.box3');
+  var $box3 = $('.box3');
+  var $count3 = $('.count3');
   var $button = $('.button');
+  var count = 1;
 
   $button.on('click', function() {
-    $box.toggleClass('active');
+    $box3.toggleClass('active');
+  });
+  $box3.on('transitionend', function(e) {
+    if (e.originalEvent.propertyName === 'width') {
+      $count3.text(count++);
+    }
+  });
+
+  var $box4 = $('.box4');
+  var $count4 = $('.count4');
+  var count = 1;
+
+  $box4.on('transitionend', function(e){
+    if (e.originalEvent.propertyName === 'width') {
+      $count4.text(count++);
+    }
   });
 }
